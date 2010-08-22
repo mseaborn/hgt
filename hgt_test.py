0dad865346effac0ef8da75630f438d56bf07369
Mark Seaborn <mrs@mythic-beasts.com>
2010-08-22 13:20:39 +0100

    Add test that covers hgt-init/hgt-record/hgt-checkout

--- hgt_test.py
+++ hgt_test.py
@@ -1,11 +1,49 @@
 
 import StringIO
+import os
+import shutil
+import subprocess
+import tempfile
 import unittest
 
 import hgtlib
 
 
-class Test(unittest.TestCase):
+def read_file(filename):
+    fh = open(filename, "r")
+    try:
+        return fh.read()
+    finally:
+        fh.close()
+
+
+def write_file(filename, data):
+    fh = open(filename, "w")
+    try:
+        fh.write(data)
+    finally:
+        fh.close()
+
+
+# http://lackingrhoticity.blogspot.com/2008/11/tempdirtestcase-python-unittest-helper.html
+class TempDirTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self._on_teardown = []
+
+    def make_temp_dir(self):
+        temp_dir = tempfile.mkdtemp(prefix="tmp-%s-" % self.__class__.__name__)
+        def tear_down():
+            shutil.rmtree(temp_dir)
+        self._on_teardown.append(tear_down)
+        return temp_dir
+
+    def tearDown(self):
+        for func in reversed(self._on_teardown):
+            func()
+
+
+class ParsingTest(unittest.TestCase):
 
     def test_parse(self):
         data = """
@@ -32,6 +70,98 @@ Patch 2345 desc 2
             {'commit_id': '2345', 'msg': 'desc 2'}]
         self.assertEquals(patches, expect)
         self.assertEquals(start, "git-svn")
+
+
+def hgt_cmd(name):
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)), name)
+
+
+test_revisions = [
+    ("Initial commit", r"""
+#include <stdio.h>
+
+int main() {
+  return 0;
+}
+"""),
+    ("Add friendly message", r"""
+#include <stdio.h>
+
+int main() {
+  printf("hello\n");
+  return 0;
+}
+"""),
+    ("Use standard message", r"""
+#include <stdio.h>
+
+int main() {
+  printf("hello world\n");
+  return 0;
+}
+"""),
+]
+
+
+class ToolTest(TempDirTestCase):
+
+    def test(self):
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        temp_dir = self.make_temp_dir()
+
+        def run_cmd(args):
+            env = os.environ.copy()
+            env["PATH"] = "%s:%s" % (script_dir, env["PATH"])
+            subprocess.check_call(args, cwd=temp_dir, env=env)
+
+        run_cmd(["git", "init", "-q"])
+        run_cmd([hgt_cmd("hgt-init")])
+        test_file = os.path.join(temp_dir, "hello.c")
+        for i, (msg, data) in enumerate(test_revisions):
+            write_file(test_file, data)
+            run_cmd(["git", "add", test_file])
+            if i == 0:
+                # Git treats the first commit specially.
+                run_cmd(["git", "commit", "-m", "Initial revision"])
+                run_cmd(["git", "tag", "git-svn"])
+            else:
+                run_cmd(["hgt-record", "-a", "-m", msg])
+
+        # Test that hgt-checkout works, and that newly-recorded
+        # patches are enabled by default.
+        run_cmd(["hgt-checkout"])
+        self.assertEquals(read_file(test_file), test_revisions[-1][1])
+
+        patches_file = os.path.join(temp_dir, ".git", "hgt-patches")
+        patches, start_point = hgtlib.parse_file(open(patches_file))
+        # This assertion just tests the default.
+        self.assertEquals(start_point, "git-svn")
+        self.assertEquals([patch["msg"] for patch in patches],
+                          ["Add friendly message",
+                           "Use standard message"])
+
+        # Test selection that causes a conflict.
+        dotgit_dir = os.path.join(temp_dir, ".git")
+        hgtlib.save_applylist(dotgit_dir,
+                              [(patches[0]["commit_id"], False, "")])
+        run_cmd(["hgt-checkout"])
+        # Without -C, the working copy is left clean and there are no
+        # conflict markers.
+        self.assertEquals(read_file(test_file), test_revisions[0][1])
+        # -C leaves in the conflict markers.
+        run_cmd(["hgt-checkout", "-C"])
+        proc = subprocess.Popen(["git", "diff"], cwd=temp_dir,
+                                stdout=subprocess.PIPE)
+        diff = proc.communicate()[0]
+        self.assertEquals(proc.wait(), 0)
+        diffs = [line for line in diff.split("\n")
+                 if line.startswith("+")
+                 and not line.startswith("+++ ")
+                 and not "<<<<<<<" in line
+                 and not ">>>>>>>" in line]
+        self.assertEquals(diffs,
+                          ['++=======',
+                           '+   printf("hello world\\n");'])
 
 
 if __name__ == "__main__":
